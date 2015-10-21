aeb0683201f026e9910facfd769b893a5d97f669
deepakiam <luckyme_deepak@yahoo.co.in>
2015-10-21 11:24:52 -0400

    changed the RED algo code to real code. extracted the constants and placed it in header file no more pseudo code in RED.c

--- RED.c
+++ RED.c
@@ -6,6 +6,7 @@
 struct timeval q_idle_time_start = 0;	//global constant so that it can be used in all functions
 node* drop_pack = head;		//constant used in drop_packet function
 
+
 long get_idle_time_interval(){
 	struct timeval curr_time_ms = gettimeofday(NULL, NULL); 	//gives microsecond value, which is what we might require/
 	return (curr_time_ms - q_idle_time_start); 
@@ -14,10 +15,12 @@ long get_idle_time_interval(){
 void enqueue(node* node){
 	if(head == NULL && tail== NULL){
 		head = tail = node;
+		queue_size++;
 		return;
 	}
 	node->next=tail;
 	tail=node;
+	queue_size
 }
 
 void dequeue(){
@@ -26,18 +29,24 @@ void dequeue(){
 	}
 	if(head == tail){
 		head = tail = NULL;
+		queue_size--;
 		q_idle_time_start = gettimeofday(NULL, NULL);		//re-initialize idle time to current time
 		return;
 	}
 	node* head_node = head;
 	head = head->next;
 	free(head_node);
+	queue_size--;
 }
 
 void drop_packet (){
 	//the drop_pack pointer will continue to point to the same memory location, 
 	//so we dont have to traverse the whole list again and again where we already deleted the marked packets.
-	int* temp = drop_pack->next;
+	if (head == NULL)
+		return;
+	else if(drop_pack == NULL)
+		drop_pack = head;
+	node* temp = drop_pack->next;
 	while(temp != NULL)
 	{
 		if (temp->marked == true)
@@ -45,6 +54,7 @@ void drop_packet (){
 			drop_pack->next = temp->next;
 			free(temp);
 			temp = drop_pack->next;
+			queue_size--;
 		}
 		else
 		{
@@ -56,43 +66,50 @@ void drop_packet (){
 }
 
 node* red(sk_buff* packet){
-	int m, constant, queue_size, minthr, maxthr, randm;
-        float pa, pb, wq, maxpb;
+	int m, randm;
+	
+	node* new_node = (node*)malloc(sizeof(node));
+	new_node->packet = packet;
+	new_node->marked = 0;				//default marking is false
+	new_node->next = NULL;
 
-                if (non-empty queue)
+                if (head == NULL && tail == NULL)
+						return new_node;
+				else if (head != NULL && tail != NULL)
                         avg_queue_size = ((1-wq)*avg_queue_size) + (wq * queue_size);
                 else
                 {
                         int m = constant * (time - q_idle_time_start);
                         avg_queue_size = ((1 - wq) ^ m) * avg_queue_size;
-                }
+					
+				}
+                
                 if (minthr <= avg_queue_size && avg_queue_size < maxthr)
                 {
-                        packaet_count++;
-                        pb = maxpb * ((avg_queue_size - minthr)/(maxthr - minthr));
-                        pa = pb / (1 - (packet_count * pb));
+                      		packet_count++;
+                     		pb = maxpb * ((avg_queue_size - minthr)/(maxthr - minthr));
+                       		pa = pb / (1 - (packet_count * pb));
 
-                        randm = rand() % 100;
+                      		randm = rand() % 100;
 
-                        if (randm <= (pa * 100))
-                        {
-                                mark the arriving packet;
-                                packet_count = 0;
-                        }
+                      		if (randm <= (pa * 100))
+                       		{
+                              		new_node->marked = 1;	//marked the packet for deletion
+                               		packet_count = 0;
+                       		}
                 }
                 else if (maxthr <= avg_queue_size)
                 {
-                        mark the arriving packet;
-                        packet_count = 0;
+                        
+							new_node->marked = 1;				//marked the packet for deletion
+							packet_count = 0;
                 }
                 else
                 {
-                        packet_count = -1;
+                      		packet_count = -1;
                 }
-	node* new_node = (node*)malloc(sizeof(node));
-	new_node->packet = packet;
-	new_node->marked = 0;				//todo marking
-	new_node->next = NULL;
+				
+	
 	return new_node;
 }
 
--- Red.h
+++ Red.h
@@ -10,6 +10,26 @@ bool marked;
 struct node* next;
 } node;
 
+
+/*variables used in RED.c*/
+
+int avg_queue_size = 0;			//the average queue size
+int queue_size = 0;				//current queue size
+int packet_count = -1; 			//number of packets since last marked packet
+float pa = 0.0;					//probability values for marking the packets for dropping
+float pb = 0.0;
+float maxpb = 0.7;				//dummy value for maximum probability with which the packets 
+								//will be marked if the average queue size is between the two threshold values
+float wq = 0.1;					//weight given to current queue size while calculating average queue size
+
+node* head, tail, drop_pack;
+
+#define maxthr		50			//dummy value for maximum threshold of packets in the buffer (used while marking the packets for deletion)
+#define minthr		10			//dummy value for minimum threshold of packets in the buffer (used while marking the packets for deletion)
+#define constant	10			//dummy value for the constant for linear function of difference between current time and queue idle time (used in red.c)
+
+
+
 void enqueue(node* node);
 
 void dequeue();
