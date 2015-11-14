bfb64284eaf56412838df761566449d9d85512e3
Akash Verma <akash1630@gmail.com>
2015-11-14 16:58:23 -0500

    Second hok added

--- AQMCallback.c
+++ AQMCallback.c
@@ -150,8 +150,17 @@ static unsigned int aqm_hook(unsigned int hooknum, struct sk_buff *skb, const st
 	return NF_ACCEPT;			   //after processing the packet, return NF_ACCEPT to let the packet pass
 }
 
+static unsigned int aqm_hook2(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in,
+                       const struct net_device *out, int (*okfn)(struct sk_buff *)){
+	printk(KERN_INFO "post routing hook\n");
+	dequeue();
+	return NF_ACCEPT;			   //after processing the packet, return NF_ACCEPT to let the packet pass
+}
+
 /* Register the hook function */
 static struct nf_hook_ops nfhops;
+
+static struct nf_hook_ops nfhops2;
 
 /* Kernel module init handler */
 //static int __init myinit(void);
@@ -263,6 +272,11 @@ int init_module(void){
 	nfhops.hooknum = 2;			//callback for this hook
 	nfhops.pf = NFPROTO_IPV4; 			//protocol is IPv4
 	nf_register_hook(&nfhops);
+	nfhops2.hook = aqm_hook2;				//hook function
+	nfhops2.priority = NF_IP_PRI_FIRST;		//function registered for hughest priority
+	nfhops2.hooknum = 3;			//callback for this hook
+	nfhops2.pf = NFPROTO_IPV4; 			//protocol is IPv4
+	nf_register_hook(&nfhops2);
 	return 0;
 }
 
@@ -270,6 +284,7 @@ int init_module(void){
 //static int __exit void my_exit(void)
 void cleanup_module(void){
     nf_unregister_hook(&nfhops);
+    nf_unregister_hook(&nfhops2);
 	printk(KERN_INFO "queue size served: %d\n", queue_size);
 	printk(KERN_INFO "number of total packets dropped : %d\n", packets_dropped);
 	printk(KERN_INFO "module unloaded from kernel!");
