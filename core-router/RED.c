0086568b8e7c324a197b4ae21968ad89a5997b23
root <akash1630@gmail.com>
2015-11-22 19:38:05 -0500

    printk added

--- core-router/RED.c
+++ core-router/RED.c
@@ -136,9 +136,10 @@ struct q_node* red(struct sk_buff* packet, long minth, long  maxth, long wq, lon
 				if((packet_count * pb) < 100){
                        			pa = (100*pb)/(100 - (packet_count * pb));
 				} else {
+					printk(KERN_INFO "setting pa = pb\n");
 					pa = pb;
 				}
-				printk(KERN_INFO "pa :%lu \n", pa);
+				//printk(KERN_INFO "pa :%lu \n", pa);
                       		randm = get_random_number();
 				printk(KERN_INFO "random number : %lu  pb : %lu  pa : %lu \n", randm, pb, pa);
                       		if (randm < pa)
